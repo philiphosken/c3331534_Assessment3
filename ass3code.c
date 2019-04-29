69a0ffaca9b506f625cb75d38a3e122279b0190b
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-29 02:19:13 +0000

    task 5 kind of words but isnt ideal..hopefully gets some marks...also this is my commit for the day

--- ass3code.c
+++ ass3code.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
 int selectionnumber = 0;
 
 printf("Would you like use:\n\nRotation Encrytion with key: 1\nRotation Decryption with key: 2\nSubstitution Encryption with key: 3\nSubstitution Decryption with key: 4\nRotation Decryption NO KEY: 5\nSubstitution Decryption NO KEY: 6\n\n\n\n");
@@ -12,28 +11,22 @@ scanf("%d", &selectionnumber);
 //      1..........Rotation encrytion with key
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
-
 if(selectionnumber == 1){
 
 int k = 0; //KEY = the number of charcters shifted in the rotation cypher to generate coded message 
 printf("(Key is the number of characters youd like the encryption to shift)\n\nKey: ");
 scanf("%d", &k);
 
-    FILE *in;
-    FILE *out;
+FILE *in;
+FILE *out;
     
-    in = fopen("in.txt", "a+"); /*  Opens a file called 'in.txt' for reading and appending (writing at end of file).  The file is created if it does not exist.  The initial
+in = fopen("in.txt", "a+"); /*  Opens a file called 'in.txt' for reading and appending (writing at end of file).  The file is created if it does not exist.  The initial
               file position for reading is at the beginning of the file, but output is always appended to the end of the file. */
-
-    out = fopen("out.txt", "w+");
+out = fopen("out.txt", "w+");
     
     while (feof(in) == 0){
-        char phi;
-        fscanf(in, "%c", &phi);
-      
-        if (feof(in)!=0){
-            fprintf(in, "%s", " "); ///One the last character is reached add a space so the last character isnt doubled
-        }
+    char phi;
+    fscanf(in, "%c", &phi);
 
         if (phi == 32){
         fprintf(out, "%c", phi); //Just prints the space
@@ -53,32 +46,26 @@ scanf("%d", &k);
         }
     }
 }
-   
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
 //      2..........Rotation Decryption with key
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
-
 else if (selectionnumber == 2){
     
 int k = 0; //KEY = the number of charcters shifted in the rotation cypher to generate coded message 
-printf("(Key is the number of characters youd like the encryption to shift)\n\nKey: ");
+printf("(Key is the number of characters that have been shifted from original)\n\nKey: ");
 scanf("%d", &k);
 
-    FILE *in;
-    FILE *out;
-    
-    in = fopen("in.txt", "a+"); /// Tha a+ allows for reading and writing to the end of a file (so i dont get two of the last character scan ABC print ABCC)
-    out = fopen("out.txt", "w+");
+FILE *in;
+FILE *out;
+
+in = fopen("in.txt", "a+"); /// Tha a+ allows for reading and writing to the end of a file (so i dont get two of the last character scan ABC print ABCC)
+out = fopen("out.txt", "w+");
     
     while (feof(in) == 0){
-        char phi;
-        fscanf(in, "%c", &phi);
-        
-        if (feof(in)!=0){
-            fprintf(in, "%s", " "); ///One the last character is reached add a space so the last character isnt doubled
-        }
+    char phi;
+    fscanf(in, "%c", &phi);
       
         if (phi == 32){
         fprintf(out, "%c", phi); //Just prints the space
@@ -98,28 +85,23 @@ scanf("%d", &k);
 
     } 
 }
-
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
 //      3..........Substitution Encryption with key
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
-
 else if (selectionnumber == 3){
     
 char alphatxt[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-char subtxt[27] ; //Keeps 27 'boxes' of space someone in memory that the user input will sub for the alphabet
+char subtxt[27] ; //Keeps 27 'boxes' of space someone in memory that the user input will sub for the alphabet PLUS SPACE 
 char N = 27;
 char m;
 char i;
 char a;
 char b;
-char str;
 
-printf("What would you like to substitute the letters of the alphatbet too?\n\nABCDEFGHIJKLMNOPQRSTUVWXYZ <space> \n\n");
+printf("What would you like to substitute the letters of the alphatbet too?\n\nABCDEFGHIJKLMNOPQRSTUVWXYZ<SPACE>\n\n");
 scanf("%s", subtxt);
-
-   
 
 FILE *in;
 FILE *out;
@@ -128,28 +110,25 @@ in = fopen("in.txt", "a+");
 out = fopen("out.txt", "w+");
     
     while (feof(in) == 0){
-        fscanf(in, "%c", &m);
+    fscanf(in, "%c", &m);
         
         for (i = 0; i < N; ++i){
         a = alphatxt[i];
         b = subtxt[i];
-            
-            if (m == a){
-            fprintf(out, "%c", b);
-            printf("%c", b);
-    } 
-       
-}
+        
+        if (m == a){
+        fprintf(out, "%c", b);
+        printf("%c", b);
+        }
 
+    }
 }
 }
-
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
 //      4..........Substitution Decryption with key
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
-
 else if (selectionnumber == 4){
 
 char alphatxt[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
@@ -157,14 +136,11 @@ char subtxt[27] ; //Keeps 27 'boxes' of space someone in memory that the user in
 char N = 27;
 char m;
 char i;
-char a;
 char b;
-char str;
+char a;
 
 printf("What have the letters of the alphabet been substituted for?\n\nABCDEFGHIJKLMNOPQRSTUVWXYZ <space> \n\n");
 scanf("%s", subtxt);
-
-   
 
 FILE *in;
 FILE *out;
@@ -173,7 +149,7 @@ in = fopen("in.txt", "a+");
 out = fopen("out.txt", "w+");
     
     while (feof(in) == 0){
-        fscanf(in, "%c", &m);
+    fscanf(in, "%c", &m);
         
         for (i = 0; i < N; ++i){
         a = alphatxt[i];
@@ -187,31 +163,27 @@ out = fopen("out.txt", "w+");
 }
 }    
 }
-
 //*********************************************************************************************************************************
 //*****************************************************************************************************************************
 //      5..........Rotation Decryption NO KEY
 //*****************************************************************************************************************************
 //*****************************************************************************************************************************
-
 else if (selectionnumber == 5){
-    
- int k = 0; //KEY = the number of charcters shifted in the rotation cypher to generate coded message 
- int yor = 0;
+int k = 0; //KEY = the number of charcters shifted in the rotation cypher to generate coded message 
+int option = 0;
 
-    FILE *in;
-    FILE *out;
-    
-    in = fopen("in.txt", "a+"); /// Tha a+ allows for reading and writing to the end of a file (so i dont get two of the last character scan ABC print ABCC)
-    out = fopen("out.txt", "w+");
+
+
+
+FILE *in;
+FILE *out;
+
+in = fopen("in.txt", "a+"); /// Tha a+ allows for reading and writing to the end of a file (so i dont get two of the last character scan ABC print ABCC)
+out = fopen("out.txt", "w+");
     
     while (feof(in) == 0){
-        char phi;
-        fscanf(in, "%c", &phi);
-        
-        if (feof(in)!=0){
-            fprintf(in, "%s", " "); ///One the last character is reached add a space so the last character isnt doubled
-        }
+    char phi;
+    fscanf(in, "%c", &phi);
       
         if (phi == 32){
         fprintf(out, "%c", phi); //Just prints the space
@@ -229,28 +201,62 @@ else if (selectionnumber == 5){
             printf("%c", phi + k);
         }
 
-        printf("is this a correct decryption? y/n :\n\n");
-        scanf("%d", yor);
-        if(yor == y){
-            break;
-        }else if 
-        k++;
-        
-    } 
-    if (k == 26){
-        printf("This message is not enccypted with a rotation cypher");
-    } else 
-}
-//*****************************************************************************************************************************
-//*****************************************************************************************************************************
-//      5..........Substitution Decryption NO KEY
-//*****************************************************************************************************************************
-//*****************************************************************************************************************************
+    }
+    fclose(in);
+    fclose(out);
+    
 
+    for (k = 1 ; k < 26 ; k++){
+    
+    printf("\n\nis this the correct decryption? (KEY = %d) y/n\n\n", k);
+    scanf("%d", &option);
+    
+    if(option == 121){
+        break;
+    } else {
+
+FILE *in;
+FILE *out;
+
+in = fopen("in.txt", "a+"); /// Tha a+ allows for reading and writing to the end of a file (so i dont get two of the last character scan ABC print ABCC)
+out = fopen("out.txt", "w+");
+    
+    while (feof(in) == 0){
+    char phi;
+    fscanf(in, "%c", &phi);
+      
+        if (phi == 32){
+        fprintf(out, "%c", phi); //Just prints the space
+        printf("%c", phi);
+    
+        } else if((phi + k) > 90){ // If it exceeds 'Z' after k is added, then simply take 26 away then add k
+        fprintf(out, "%c", phi - 26 + k); 
+        printf("%c", phi - 26 + k);
+        
+        } else if ( (phi + k) < 65 ){ //If it exceeds 'Z' after k is added, then simply add 26 away then add k
+        fprintf(out, "%c", phi + 26 + k);
+        printf("%c", phi + 26 + k);
+            
+        } else {fprintf(out, "%c", phi + k);// if within the bounds of ASCII 'A' and 'Z' just add k
+            printf("%c", phi + k);
+        }
+
+    }
+
+}
+fclose(in);
+fclose(out);
+}
+   
+}       
+//*****************************************************************************************************************************
+//*****************************************************************************************************************************
+//      6..........Substitution Decryption NO KEY
+//*****************************************************************************************************************************
+//*****************************************************************************************************************************
 else if (selectionnumber == 6){
-    
-    
-    
+
+printf("ran out of time yet I have a basic understanding on how this concept could work");
     
 }
 } //final closing brace...end of program yaya
